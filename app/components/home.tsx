9bc25775497cb9f3fe8b286af54e89b7585113da
ozatechnologies <100591237+ozatechnologies@users.noreply.github.com>
2023-06-04 19:06:35 +0530

    Update home.tsx

--- app/components/home.tsx
+++ app/components/home.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import { SideBar } from "./sidebar";

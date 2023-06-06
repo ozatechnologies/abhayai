32bb7bb4eb2aec2e99a617b71b0c68fa08568ee6
ozatechnologies <100591237+ozatechnologies@users.noreply.github.com>
2023-06-06 13:33:39 +0530

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -98,7 +98,7 @@ const handleMouseUp = useRef(() => {
   };
 }
 
-export function SideBar(props: SideBarProps) {
+export function SideBar(props: { className?: string }) {
   const chatStore = useChatStore();
 
   // drag side bar
